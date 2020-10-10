e84c79355d6cc4a613c2ad079642bede4633690a
ochHerrje <och.herrje@gmail.com>
2020-10-11 01:38:26 +0200

    Added a bunch of functionality, such as writing to and reading from files directly, RAII-wrappers for handles and simple creation of temporary ("scratchpad") files, which are not only closed but also deleted upon leaving scope. Also reworked functions to use och::string instead of const char*

--- och_lib/och_fio.cpp
+++ och_lib/och_fio.cpp
@@ -56,75 +56,106 @@ namespace och
 		return (access_rights - ((access_rights == 3) << 1)) << 1;
 	}
 
-	iohandle open_file(const char* filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode)
+	iohandle open_file(const och::string filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode)
 	{
-		void* file = CreateFileA(filename, access_interp_open(access_rights), share_mode, nullptr, interp_openmode(existing_mode, new_mode), FILE_ATTRIBUTE_NORMAL, nullptr);
+		iohandle file = CreateFileA(filename.begin(), access_interp_open(access_rights), share_mode, nullptr, interp_openmode(existing_mode, new_mode), FILE_ATTRIBUTE_NORMAL, nullptr);
 
 		return file == INVALID_HANDLE_VALUE ? nullptr : file;
 	}
 
-	iohandle open_file(const och::string filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode)
+	iohandle create_file_mapper(const iohandle file, uint64_t size, uint32_t page_mode, const char* mapping_name)
 	{
-		return open_file(filename.begin(), access_rights, existing_mode, new_mode, share_mode);
-	}
-
-	iohandle create_file_mapper(iohandle file, uint64_t size, uint32_t page_mode, const char* mapping_name)
-	{
-		if (!file._ptr)
-			return iohandle{ nullptr };
+		if (!file)
+			return nullptr;
 
 		LARGE_INTEGER _size;
 
 		_size.QuadPart = size;
 
-		return CreateFileMappingA(file._ptr, nullptr, access_interp_page(page_mode), _size.HighPart, _size.LowPart, mapping_name);
+		return CreateFileMappingA(file, nullptr, access_interp_page(page_mode), _size.HighPart, _size.LowPart, mapping_name);
 	}
 
-	iohandle file_as_array(iohandle file_mapping, uint32_t filemap_mode, uint64_t beg, uint64_t end)
+	iohandle file_as_array(const iohandle file_mapping, uint32_t filemap_mode, uint64_t beg, uint64_t end)
 	{
 		LARGE_INTEGER _beg;
 
 		_beg.QuadPart = beg;
 
-		return MapViewOfFile(file_mapping._ptr, access_interp_fmap(filemap_mode), _beg.HighPart, _beg.LowPart, end - beg);
+		return MapViewOfFile(file_mapping, access_interp_fmap(filemap_mode), _beg.HighPart, _beg.LowPart, end - beg);
 	}
 
-	uint32_t readbytes(iohandle file, char* dst, uint32_t bytes)
+	bool set_fileptr(const iohandle file, int64_t set_to, uint32_t setptr_mode)
+	{
+		LARGE_INTEGER _set_to;
+
+		_set_to.QuadPart = set_to;
+
+		return SetFilePointerEx(file, _set_to, nullptr, static_cast<DWORD>(setptr_mode));
+	}
+
+	uint32_t freadbytes(const iohandle file, char* dst, uint32_t bytes)
 	{
 		uint32_t bytes_read = 0;
 
-		ReadFile(file._ptr, dst, bytes, reinterpret_cast<LPDWORD>(&bytes_read), nullptr);
+		ReadFile(file, dst, bytes, reinterpret_cast<LPDWORD>(&bytes_read), nullptr);
 
 		return bytes_read;
 	}
 
-	int64_t get_filesize(iohandle file)
+	uint32_t fwritebytes(const iohandle file, const char* src, uint32_t bytes)
+	{
+		uint32_t bytes_written = 0;
+
+		WriteFile(file, reinterpret_cast<const void*>(src), bytes, reinterpret_cast<LPDWORD>(&bytes_written), nullptr);
+
+		return bytes_written;
+	}
+
+	int64_t get_filesize(const iohandle file)
 	{
 		LARGE_INTEGER filesize;
 
-		GetFileSizeEx(file._ptr, &filesize);
+		GetFileSizeEx(file, &filesize);
 
 		return filesize.QuadPart;
 	}
 
-	bool close_file(iohandle file)
+	bool close_file(const iohandle file)
 	{
-		return CloseHandle(file._ptr);
+		return CloseHandle(file);
 	}
 
-	bool close_file_array(iohandle file_array)
+	bool close_file_array(const iohandle file_array)
 	{
-		return UnmapViewOfFile(file_array._ptr);
-	}
-
-	bool delete_file(const char* filename)
-	{
-		return DeleteFileA(filename);
+		return UnmapViewOfFile(file_array);
 	}
 
 	bool delete_file(const och::string filename)
 	{
-		return delete_file(filename.begin());
+		return DeleteFileA(filename.begin());
+	}
+
+	iohandle create_tempfile()
+	{
+		char filename[MAX_PATH];
+
+		if (!GetTempFileNameA(".", "och", 0, filename))
+			return nullptr;
+
+		return open_file(filename, fio::access_readwrite, fio::open_normal, fio::open_fail);
+	}
+
+	tempfilehandle::tempfilehandle() : handle{ create_tempfile() } {}
+
+	tempfilehandle::~tempfilehandle()
+	{
+		char buf[MAX_PATH];
+
+		GetFinalPathNameByHandleA(handle, buf, MAX_PATH, 0);
+
+		close_file(handle);
+
+		delete_file(buf);
 	}
 }
 
--- och_lib/och_fio.h
+++ och_lib/och_fio.h
@@ -5,54 +5,95 @@
 
 namespace och
 {
-	struct iohandle
-	{
-		void* _ptr = nullptr;
-
-		iohandle(void* file_ptr) : _ptr{ file_ptr } {}
-	};
-
 	namespace fio
 	{
-		constexpr uint32_t open_normal			= 0;
-		constexpr uint32_t open_truncate		= 1;
-		constexpr uint32_t open_fail			= 2;
+		enum _fio : uint32_t
+		{
+			open_normal = 0,
+			open_truncate = 1,
+			open_fail = 2,
 
-		constexpr uint32_t access_read			= 2;
-		constexpr uint32_t access_write			= 1;
-		constexpr uint32_t access_readwrite		= 3;
+			access_read = 2,
+			access_write = 1,
+			access_readwrite = 3,
 
-		constexpr uint32_t share_none			= 0;
-		constexpr uint32_t share_read			= 1;
-		constexpr uint32_t share_write			= 2;
-		constexpr uint32_t share_delete			= 4;
+			share_none = 0,
+			share_read = 1,
+			share_write = 2,
+			share_delete = 4,
+
+			setptr_beg = 0,
+			setptr_cur = 1,
+			setptr_end = 2,
+		};
+
 	}
+
+	using iohandle = void*;
 
 	iohandle open_file(const och::string filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode = fio::share_none);
 
-	iohandle open_file(const char* filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode = fio::share_none);
+	iohandle create_file_mapper(const iohandle file, uint64_t size, uint32_t access_rights, const char* mapping_name = nullptr);
 
-	iohandle create_file_mapper(iohandle file, uint64_t size, uint32_t access_rights, const char* mapping_name = nullptr);
+	iohandle file_as_array(const iohandle file_mapper, uint32_t access_rights, uint64_t beg, uint64_t end);
 
-	iohandle file_as_array(iohandle file_mapper, uint32_t access_rights, uint64_t beg, uint64_t end);
+	int64_t get_filesize(const iohandle file);
 
-	int64_t get_filesize(iohandle file);
-
-	uint32_t readbytes(iohandle file, char* dst, uint32_t bytes);
+	uint32_t freadbytes(const iohandle file, char* dst, uint32_t bytes);
 
 	template<typename T>
-	och::memrun<T> read_from_file(iohandle file, och::memrun<T> dst)
+	och::memrun<T> read_from_file(const iohandle file, och::memrun<T> dst)
 	{
-		return { dst.begin(), readbytes(file, reinterpret_cast<char*>(dst.begin()), static_cast<uint32_t>(dst.len())) };
+		return och::memrun(dst.begin(), dst.begin() + freadbytes(file, reinterpret_cast<char*>(dst.begin()), static_cast<uint32_t>(dst.len() * sizeof(T))));
 	}
 
-	bool close_file(iohandle file);
+	uint32_t fwritebytes(const iohandle file, const char* src, uint32_t bytes);
 
-	bool close_file_array(iohandle file_array);
+	template<typename T>
+	bool write_to_file(const iohandle file, och::memrun<T> src)
+	{
+		return fwritebytes(file, src.begin(), static_cast<uint32_t>(src.len() * sizeof(T)));
+	}
+
+	bool close_file(const iohandle file);
+
+	bool close_file_array(const iohandle file_array);
 
 	bool delete_file(const och::string filename);
 
-	bool delete_file(const char* filename);
+	bool set_fileptr(const iohandle file, int64_t set_to, uint32_t setptr_mode);
+
+	struct filehandle
+	{
+		const iohandle handle;
+
+		filehandle(const och::string filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t share_mode = fio::share_none) : handle{ open_file(filename, access_rights, existing_mode, new_mode, share_mode) } {}
+
+		filehandle(iohandle handle) : handle{ handle } {}
+
+		~filehandle() { close_file(handle); }
+
+		template<typename T> och::memrun<T> read(och::memrun<T> dst) const { return read_from_file(handle, dst); }
+
+		template<typename T> bool write(const och::memrun<T> src) const { return write_to_file(handle, src); }
+
+		bool set_fileptr(int64_t set_to, uint32_t setptr_mode = fio::setptr_beg) const { return och::set_fileptr(handle, set_to, setptr_mode); }
+	};
+
+	struct tempfilehandle
+	{
+		const iohandle handle;
+
+		tempfilehandle();
+
+		~tempfilehandle();
+
+		template<typename T> och::memrun<T> read(och::memrun<T> dst) const { return read_from_file(handle, dst); }
+
+		template<typename T> bool write(const och::memrun<T> src) const { return write_to_file(handle, src); }
+
+		bool set_fileptr(int64_t set_to, uint32_t setptr_mode = fio::setptr_beg) const { return och::set_fileptr(handle, set_to, setptr_mode); }
+	};
 
 	template<typename T>
 	struct file_array
@@ -67,11 +108,18 @@ namespace och
 
 		const uint32_t size;
 
-		file_array(const char* filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t mapping_size, uint32_t mapping_offset = 0) :
+		file_array(const och::string filename, uint32_t access_rights, uint32_t existing_mode, uint32_t new_mode, uint32_t mapping_size, uint32_t mapping_offset = 0) :
 			file{ open_file(filename, access_rights, existing_mode, new_mode) },
 			mapper{ create_file_mapper(file, (uint64_t)mapping_size + mapping_offset, access_rights) },
 			data{ file_as_array(mapper, access_rights, mapping_offset, (uint64_t) mapping_offset + mapping_size) },
 			size{ mapping_size == 0 ? (uint32_t) get_filesize(file) : mapping_size }
+		{}
+
+		file_array(const iohandle file, uint32_t access_rights, uint32_t mapping_size, uint32_t mapping_offset = 0) :
+			file{ file },
+			mapper{ create_file_mapper(file, (uint64_t)mapping_size + mapping_offset, access_rights) },
+			data{ file_as_array(mapper, access_rights, mapping_offset, (uint64_t)mapping_offset + mapping_size) },
+			size{ mapping_size == 0 ? (uint32_t)get_filesize(file) : mapping_size }
 		{}
 
 		~file_array()
@@ -82,6 +130,11 @@ namespace och
 		}
 
 		T& operator[](size_t i)
+		{
+			return reinterpret_cast<T*>(data)[i];
+		}
+
+		const T& operator[](size_t i) const
 		{
 			return reinterpret_cast<T*>(data)[i];
 		}
