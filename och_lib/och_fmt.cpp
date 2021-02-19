1d4033faa70eb7c5757fb191a4c07018fe1a3208
ochHerrje <och.herrje@gmail.com>
2021-02-19 22:11:40 +0100

    Started reworking och_fmt

--- och_lib/och_fmt.cpp
+++ och_lib/och_fmt.cpp
@@ -7,547 +7,149 @@
 
 namespace och
 {
-	constexpr const char f_hex_symbols[2][16]{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-	const char* f_curr_hex_symbols = f_hex_symbols[0];
+	arg_wrapper::arg_wrapper(uint64_t value, fmt_fn format_function) : value{ value }, format_function{ format_function } {}
 
-	constexpr int vprint_buf_size = 8192;
-
-	char _vbuf[vprint_buf_size];
-	och::range vbuf(_vbuf);
-
-	inline void to_vbuf(char c, iohandle out)
+	arg_wrapper wrap_fmt_arg(uint64_t value)
 	{
-		if (!vbuf.len())
-		{
-			och::write_to_file(out, { vbuf.end - vprint_buf_size, vbuf.end });
-			vbuf.beg = _vbuf;
-		}
-
-		*(vbuf.beg++) = c;
+		return arg_wrapper{ value, nullptr };
 	}
 
-	inline void to_vbuf(const char* str, uint32_t len, iohandle out)
+	//[width] [.precision] [rightadj] [~filler] [signmode] [format specifier]}
+	default_context::default_context(const char* context, const och::arg_wrapper* argv, const uint32_t argc)
 	{
-		while (true)
+
+		if (*context == '#')//Argument Reference
 		{
-			uint32_t to_write = len < (uint32_t)vbuf.len() ? len : (uint32_t)vbuf.len();
+			++context;
 
-			for (uint32_t i = 0; i < to_write; ++i)
-				*(vbuf.beg++) = *(str++);
+			uint32_t width_idx = 0;
 
-			len -= to_write;
-			
-			if (!len)
-				return;
+			while (*context >= '0' && *context <= '9')
+				width_idx = width_idx * 10 + *context++ - '0';
 
-			och::write_to_file(out, { vbuf.end - vprint_buf_size, vbuf.beg });
-			vbuf.beg = _vbuf;
-		}
-	}
+			_ASSERT(width_idx < argc);
 
-	uint32_t f_default_precision = 4;
-	float f_default_precision_factor = 10.0F * 10.0F * 10.0F * 10.0F;
-
-	void f_uint(arg in, och::iohandle out)
-	{
-		uint64_t val = in.i;
-
-		char buf[20];
-		int i = 19;
-
-		while (val >= 10)
-		{
-			buf[i] = '0' + val % 10;
-
-			val /= 10;
-
-			--i;
-		}
-
-		buf[i--] = '0' + static_cast<char>(val);
-
-		if (in.get_rightadj())
-		{
-			for (uint32_t j = 19 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		
-			to_vbuf(buf + i + 1, 19 - i, out);
+			width = static_cast<uint16_t>(argv[width_idx].value);
 		}
 		else
 		{
-			to_vbuf(buf + i + 1, 19 - i, out);
-
-			for (uint32_t j = 19 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_int(arg in, och::iohandle out)
-	{
-		int64_t val = static_cast<int64_t>(in.i);
-
-		char buf[20];
-		int i = 19;
-
-		bool negative = false;
-
-		if (val < 0)
-		{
-			negative = true;
-			val = -val;
+			width = 0;
+			if (*context >= '0' && *context <= '9')
+				while (*context >= '0' && *context <= '9')
+					width = width * 10 + *context++ - '0';
+			else
+				++context;
 		}
 
-		while (val >= 10)
+
+		if (*context == '.')
 		{
-			buf[i--] = '0' + val % 10;
+			++context;
 
-			val /= 10;
-		}
-
-		buf[i--] = '0' + static_cast<char>(val);
-
-		if (negative)
-			buf[i--] = '-';
-		else if (in.get_signmode() == 1)
-			buf[i--] = '+';
-		else if (in.get_signmode() == 2)
-			buf[i--] = ' ';
-
-		if (in.get_rightadj())
-		{
-			for (uint32_t j = 19 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		
-			to_vbuf(buf + i + 1, 19llu - i, out);
-		}
-		else
-		{
-			to_vbuf(buf + i + 1, 19llu - i, out);
-		
-			for (uint32_t j = 19 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_float(arg in, och::iohandle out)
-	{
-
-	}
-
-	void f_double(arg in, och::iohandle out)
-	{
-
-	}
-
-	void f_ministring(arg in, och::iohandle out)
-	{
-		uint16_t len = in.s.len();
-
-		if (in.get_precision() != -1 && len > in.get_precision())
-			len = in.get_precision();
-
-		if (in.get_rightadj())
-		{
-			for (size_t j = len; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		
-			to_vbuf(in.s.begin(), len, out);
-		}
-		else
-		{
-			to_vbuf(in.s.begin(), len, out);
-		
-			for (size_t j = len; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_character(arg in, och::iohandle out)
-	{
-		uint32_t cunits = 1;
-		char32_t cpoint = in.c;
-
-		char buf[4];
-
-		if (cpoint > 0xFFFF)
-		{
-			buf[0] = static_cast<char>(0xF0 |   cpoint >> 18         );
-			buf[1] = static_cast<char>(0x80 | ((cpoint >> 12) & 0x3F));
-			buf[2] = static_cast<char>(0x80 | ((cpoint >>  6) & 0x3F));
-			buf[3] = static_cast<char>(0x80 |  (cpoint        & 0x3F));
-
-			cunits = 4;
-		}
-		else if (cpoint > 0x07FF)
-		{
-			buf[0] = static_cast<char>(0xE0 |   cpoint >> 12         );
-			buf[1] = static_cast<char>(0x80 | ((cpoint >>  6) & 0x3F));
-			buf[2] = static_cast<char>(0x80 |  (cpoint        & 0x3F));
-
-			cunits = 3;
-		}
-		else if (cpoint > 0x007F)
-		{
-			buf[0] = static_cast<char>(0xC0 |   cpoint >> 6          );
-			buf[1] = static_cast<char>(0x80 |  (cpoint        & 0x3F));
-
-			cunits = 2;
-		}
-		else
-			buf[0] = static_cast<char>(cpoint);
-
-		if (in.get_rightadj())
-		{
-			for (uint32_t j = 1; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-
-			to_vbuf(buf, cunits, out);
-		}
-		else
-		{
-			to_vbuf(buf, cunits, out);
-
-			for (uint32_t j = 1; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_time(arg in, och::iohandle out)
-	{
-		char buf[] {50};
-
-		date d(in.t);
-
-		uint16_t year = d.year;
-
-		int i = 0;
-
-		if (year < 1000)
-			i = 3;
-		else if (year < 10000)
-			i = 4;
-		else
-			i = 5;
-
-		uint32_t idx = i + 1;
-
-		while (year >= 10)
-		{
-			buf[i] = '0' + year % 10;
-
-			year /= 10;
-
-			--i;
-		}
-
-		buf[i] = '0' + year;
-
-		buf[idx++] = '-';
-
-		buf[idx++] = '0' + (d.month >= 10);
-
-		buf[idx++] = '0' + (d.month >= 10 ? d.month - 10: d.month);
-
-		buf[idx++] = '-';
-
-		buf[idx++] = '0' + d.monthday / 10;
-
-		buf[idx++] = '0' + d.monthday % 10;
-
-		buf[idx++] = ',';
-
-		buf[idx++] = ' ';
-
-		buf[idx++] = '0' + d.hour / 10;
-
-		buf[idx++] = '0' + d.hour % 10;
-
-		buf[idx++] = ':';
-
-		buf[idx++] = '0' + d.minute / 10;
-
-		buf[idx++] = '0' + d.minute % 10;
-
-		buf[idx++] = ':';
-
-		buf[idx++] = '0' + d.second / 10;
-
-		buf[idx++] = '0' + d.second % 10;
-
-		buf[idx++] = '.';
-
-		uint32_t ms = d.millisecond;
-
-		buf[idx++] = '0' + ms / 100;
-
-		ms %= 100;
-
-		buf[idx++] = '0' + ms / 10;
-
-		buf[idx] = '0' + ms % 10;
-
-		to_vbuf(buf + (year < 10000), idx, out);
-	}
-
-	void f_hexadecimal(arg in, och::iohandle out)
-	{
-		uint64_t val = in.i;
-		
-		char buf[16];
-		int i = 15;
-		
-		if (!val)
-			buf[i--] = '0';
-		
-		while (val)
-		{
-			buf[i--] = f_curr_hex_symbols[val & 0xF];
-		
-			val >>= 4;
-		}
-		
-		if (in.get_rightadj())
-		{
-			for (uint32_t j = 15 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		
-			to_vbuf(buf + i + 1, 15llu - i, out);
-		}
-		else
-		{
-			to_vbuf(buf + i + 1, 15llu - i, out);
-		
-			for (uint32_t j = 15 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_binary(arg in, och::iohandle out)
-	{
-		uint64_t val = in.i;
-		
-		char buf[64];
-		int i = 63;
-		
-		if (!val)
-			buf[i--] = '0';
-		
-		while (val)
-		{
-			buf[i--] = '0' + (val & 1);
-		
-			val >>= 1;
-		}
-		
-		if (in.get_rightadj())
-		{
-			for (uint32_t j = 63 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		
-			to_vbuf(buf + i + 1, 63llu - i, out);
-		}
-		else
-		{
-			to_vbuf(buf + i + 1, 63llu - i, out);
-		
-			for (uint32_t j = 63 - i; j < in.get_width(); ++j)
-				to_vbuf(in.get_filler(), out);
-		}
-	}
-
-	void f_scientific(arg in, och::iohandle out)
-	{
-
-	}
-
-	//Format-function vtable
-	fmt_function format_functions[33] alignas(64) {
-		f_uint,
-		f_int,
-		f_float,
-		f_double,
-		f_ministring,
-		f_character,
-		f_time,
-		nullptr,			//a
-		f_binary,			//b
-		nullptr,			//c
-		nullptr,			//d
-		f_scientific,		//e
-		nullptr,			//f
-		nullptr,			//g
-		nullptr,			//h
-		nullptr,			//i
-		nullptr,			//j
-		nullptr,			//k
-		nullptr,			//l
-		nullptr,			//m
-		nullptr,			//n
-		nullptr,			//o
-		nullptr,			//p
-		nullptr,			//q
-		nullptr,			//r
-		nullptr,			//s
-		nullptr,			//t
-		nullptr,			//u
-		nullptr,			//v
-		nullptr,			//w
-		f_hexadecimal,		//x
-		nullptr,			//y
-		nullptr,			//z
-	};
-
-	// [argindex] [:[width] [.precision] [rightadj] [~filler] [signmode] [formatmode]]
-	void vprint(const char* fmt, arg argv[], uint32_t argc, och::iohandle out)
-	{
-		uint32_t arg_idx = 0;
-
-		for (char c = *fmt; c; c = *(++fmt))
-		{
-			if (c == '{')//Escape char encountered
+			if (*context == '#')
 			{
-				c = *(++fmt);
+				++context;
 
-				if (c == '{')						//Exit for printing escape-character
-				{
-					to_vbuf(c, out);
+				uint32_t precision_idx = 0;
 
-					continue;
-				}
+				while (*context >= '0' && *context <= '9')
+					precision_idx = precision_idx * 10 + *context++ - '0';
 
-				//Actual formatting encountered
+				_ASSERT(precision_idx < argc);
 
-				if (c >= '0' && c <= '9')					//Index
-				{
-					arg_idx = c - '0';
-					c = *(++fmt);
-				}
-
-				_ASSERT(arg_idx < argc);
-
-				arg curr_arg = argv[arg_idx];
-
-				if (c == ':')								//Formato xxxtendo
-				{
-					// [width] [precision] [rightadj] [filler] [signmode] [formatmode]
-					//
-					//		width			[0-9]*, indicating minimum field width to be padded to
-					//		precision		.[0-9]*, indicating decimal digits for float/double, or number of printable characters for strings
-					//		rightadj		'>', indicating that the formatted string shall be right-adjusted in its field
-					//		filler			~.
-					//		signmode		'+' or '_', meaning positive signed ints are padded with a plus or space respectively
-					//		formatmode		o, x, X, b, c for (u)ints, e, b for float/double
-
-					uint8_t f_width = 0;
-					while ((c = *(++fmt)) >= '0' && c <= '9')
-						f_width = f_width * 10 + c - '0';
-					curr_arg.set_width(f_width);
-
-					if (c == '.')
-					{
-						uint16_t f_precision = 0;
-						while ((c = *(++fmt)) >= '0' && c <= '9')
-							f_precision = f_precision * 10 + c - '0';
-						curr_arg.set_precision(f_precision);
-					}
-
-					if (c == '>')
-					{
-						curr_arg.set_rightadj();
-						c = *(++fmt);
-					}
-
-					if (c == '~')
-					{
-						c = *(++fmt);
-						curr_arg.set_filler(c);
-						c = *(++fmt);
-					}
-
-					if (c == '+')
-					{
-						curr_arg.set_signmode(1);
-						c = *(++fmt);
-					}
-					else if (c == '_')
-					{
-						curr_arg.set_signmode(2);
-						c = *(++fmt);
-					}
-
-					if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-					{
-						curr_arg.set_fmt_specifier(c);
-						c = *(++fmt);
-					}
-				}
-
-				_ASSERT(c == '}');
-
-				format_functions[static_cast<uint8_t>(curr_arg.get_offset())](curr_arg, out);
-
-				++arg_idx;
+				precision = static_cast<uint16_t>(argv[precision_idx].value);
 			}
 			else
 			{
-				to_vbuf(c, out);
+				precision = 0;
+
+				while (*context >= '0' && *context <= '9')
+					precision = precision * 10 + *context++ - '0';
 			}
 		}
+		else
+			precision = ~0;
 
-		if (vbuf.beg != _vbuf)
+		flags = 0;
+
+		if (*context == '>')
 		{
-			och::write_to_file(out, { _vbuf, vbuf.beg });
-			vbuf.beg = _vbuf;
+			flags |= 4;
+			++context;
+		}
+
+		if (*context == '~')
+		{
+			++context;
+
+			filler = utf8_codepoint(context);
+
+			context += filler.get_codeunits();
+		}
+
+		if (*context == '+')
+		{
+			flags |= 1;
+			++context;
+		}
+		else if (*context == '_')
+		{
+			flags |= 2;
+			++context;
+		}
+
+		if (*context != '}')
+		{
+			format_specifier = utf8_codepoint(context);
+
+			context += format_specifier.get_codeunits();
+		}
+
+		_ASSERT(*context == '}');
+	}
+
+	//{[argindex] [:[width] [.precision] [rightadj] [~filler] [signmode] [formatmode]]}
+	void vprint(const char* format, uint32_t cunits, arg_wrapper* argv, uint32_t argc, och::iohandle out)
+	{
+		uint32_t arg_counter = 0;
+
+		for (uint32_t last_fmt_end = 0, idx = 0; idx != cunits;)
+		{
+			if (format[idx++] == '{')
+			{
+				++arg_counter;
+
+				uint32_t arg_idx;
+
+				if (format[idx] >= '0' && format[idx] <= '9')
+				{
+					arg_idx = format[idx++] - '0';
+
+					while (format[idx] >= '0' && format[idx] <= '9')
+						arg_idx = arg_idx * 10 + format[idx++] - '0';
+				}
+				else
+					arg_idx = arg_counter;
+
+				_ASSERT(arg_idx < argc);
+
+				_ASSERT(format[idx] == ':' || format[idx] == '}');
+
+				if (format[idx] == ':')
+					++idx;
+
+				argv[arg_idx].format_function(out, argv[arg_idx].value, format + idx, argv, argc);
+			}
 		}
 	}
 
-	void print(const och::stringview fmt)
+	void print(const och::utf8_string& format)
 	{
-		och::write_to_file(och::out, { fmt.beg, fmt.end - 1 });
+		och::write_to_file(och::out, { format.raw_cbegin(), format.raw_cend() });
 	}
 
-	void print(och::iohandle out, const och::stringview fmt)
+	void print(const och::utf8_view& format)
 	{
-		och::write_to_file(out, { fmt.beg, fmt.end - 1 });
-	}
-
-	namespace fmt
-	{
-		void hex_lowercase()
-		{
-			f_curr_hex_symbols = f_hex_symbols[1];
-		}
-
-		void hex_uppercase()
-		{
-			f_curr_hex_symbols = f_hex_symbols[0];
-		}
-
-		void set_default_precision(uint32_t precision)
-		{
-			if (precision > 9)
-				precision = 9;
-
-			f_default_precision = precision;
-
-			float precision_factor = 1.0F;
-
-			for (uint32_t i = 0; i < precision; ++i)
-				precision_factor *= 10.0F;
-
-			f_default_precision_factor = precision_factor;
-		}
-
-		bool register_formatfunction(och::fmt_function function, char specifier)
-		{
-			if (specifier <= 'a' && specifier >= 'z')
-				return false;
-
-			uint8_t index = specifier - 'a' + 5;
-
-			format_functions[index] = function;
-
-			return true;
-		}
+		och::write_to_file(och::out, { format.m_ptr, format.m_ptr + format.m_codeunits });
 	}
 }
--- och_lib/och_fmt.h
+++ och_lib/och_fmt.h
@@ -7,105 +7,42 @@
 
 namespace och
 {
-	struct arg
+	struct arg_wrapper;
+
+	using fmt_fn = void (*) (och::iohandle out, uint64_t arg_value, const char* context, const och::arg_wrapper* argv, const uint32_t argc);
+
+	struct arg_wrapper
 	{
-		uint16_t precision = static_cast<uint16_t>(-1);
-		uint16_t flags = 0;
-		uint8_t width = 0;
-		char filler = ' ';
-		uint8_t fmt_specifier = '\0';
-		uint8_t offset;
+		uint64_t value;
 
-		union
-		{
-			uint64_t i;
-			double d;
-			float f;
-			och::mini_stringview s;
-			char32_t c;
-			och::time t;
-		};
+		fmt_fn format_function;
 
-		enum class types : uint8_t
-		{
-			_uint,
-			_int,
-			_float,
-			_double,
-			_string,
-			_char,
-			_time,
-		};
-
-		void set_width(uint8_t w) { width = w; }
-		void set_precision(uint16_t p) { precision = p; }
-		void set_rightadj() { flags |= 4; }
-		void set_signmode(uint16_t s) { flags |= s; }
-		void set_offset(uint8_t o) { offset = o; }
-		void set_filler(char f) { filler = f; }
-		void set_fmt_specifier(char s) { fmt_specifier = s; }
-
-		uint16_t get_width() { return width; }
-		uint16_t get_precision() { return precision; }
-		bool get_rightadj() { return flags & 4; }
-		uint16_t get_signmode() { return  flags & 3; }
-		uint8_t get_offset() { return offset; }
-		char get_filler() { return filler; }
-		char get_fmt_specifier() { return fmt_specifier; }
-
-		arg(                uint8_t i) : offset{ static_cast<uint8_t>(types::_uint  ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(               uint16_t i) : offset{ static_cast<uint8_t>(types::_uint  ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(               uint32_t i) : offset{ static_cast<uint8_t>(types::_uint  ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(               uint64_t i) : offset{ static_cast<uint8_t>(types::_uint  ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(                 int8_t i) : offset{ static_cast<uint8_t>(types::_int   ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(                int16_t i) : offset{ static_cast<uint8_t>(types::_int   ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(                int32_t i) : offset{ static_cast<uint8_t>(types::_int   ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(                int64_t i) : offset{ static_cast<uint8_t>(types::_int   ) }, i{                                             static_cast<uint64_t>(i) } {}
-		arg(                  float f) : offset{ static_cast<uint8_t>(types::_float ) }, f{                                                                   f  } {}
-		arg(                 double d) : offset{ static_cast<uint8_t>(types::_double) }, d{                                                                   d  } {}
-		arg(            const char* s) : offset{ static_cast<uint8_t>(types::_string) }, s{                                              s, _const_strlen_u16(s) } {}
-		arg(        och::stringview s) : offset{ static_cast<uint8_t>(types::_string) }, s{                                                                   s  } {}
-		arg(   och::mini_stringview s) : offset{ static_cast<uint8_t>(types::_string) }, s{                                                                   s  } {}
-		arg(       och::range<char> s) : offset{ static_cast<uint8_t>(types::_string) }, s{ reinterpret_cast<const char*>(s.beg), static_cast<uint16_t>(s.len()) } {}
-		arg(const och::utf8_string& s) : offset{ static_cast<uint8_t>(types::_string) }, s{                                   s.raw_cbegin(), s.get_codeunits()  } {}
-		arg(                   char c) : offset{ static_cast<uint8_t>(types::_string) }, c{                                            static_cast<char32_t>(c)  } {}
-		arg(               char32_t c) : offset{ static_cast<uint8_t>(types::_string) }, c{                                                                   c  } {}
-		arg(              och::time t) : offset{ static_cast<uint8_t>(types::_time  ) }, t{                                                                   t  } {}
+		arg_wrapper(uint64_t value, fmt_fn format_function);
 	};
 
-	using fmt_function = void (*) (arg in, och::iohandle out);
+	arg_wrapper wrap_fmt_arg(uint64_t value);
 
-	// [argindex] [:[width] [.precision] [rightadj] [~filler] [signmode] [formatmode]]
-	void vprint(const char* fmt, arg argv[], uint32_t argc, och::iohandle);
+	struct default_context
+	{
+		uint16_t precision;
+		uint16_t width;
+		uint8_t flags;
+		och::utf8_codepoint filler;
+		och::utf8_codepoint format_specifier;
 
-	void print(const och::stringview fmt);
+		default_context(const char* context, const och::arg_wrapper* argv, const uint32_t argc);
+	};
 
+	void vprint(const char* format, uint32_t cunits, arg_wrapper* argv, uint32_t argc, och::iohandle out);
+
+	//{[argindex] [:[width] [.precision] [rightadj] [~filler] [signmode] [format specifier]]}
 	template<typename... Args>
-	void print(const och::stringview fmt, Args... args)
+	void print(const och::utf8_string& format, Args... args)
 	{
-		arg argv[]{ args... };
+		arg_wrapper argv[]{ wrap_fmt_arg(args)... };
 
-		vprint(fmt.beg, argv, sizeof...(args), och::out);
+		vprint(format.raw_cbegin(), format.get_codeunits(), argv, sizeof...(args), och::out);
 	}
 
-	void print(och::iohandle out, const och::stringview fmt);
-
-	template<typename... Args>
-	void print(och::iohandle out, const och::stringview fmt, Args... args)
-	{
-		arg argv[]{ args... };
-
-		vprint(fmt.beg, argv, sizeof...(args), out);
-	}
-
-	namespace fmt
-	{
-		void hex_lowercase();
-
-		void hex_uppercase();
-
-		void set_default_precision(uint32_t precision);
-
-		bool register_formatfunction(fmt_function function, char specifier);
-	}
+	void print(const och::utf8_string& format);
 }
