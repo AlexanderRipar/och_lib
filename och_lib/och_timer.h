03f790eca66577c46a3ddf0856a32273ca799320
ochHerrje <och.herrje@gmail.com>
2021-03-16 19:25:10 +0100

    Added different timers for normal and highres_time. Previously, only the latter existed.

--- och_lib/och_timer.h
+++ och_lib/och_timer.h
@@ -3,7 +3,7 @@
 #include "och_time.h"
 namespace och
 {
-	struct timer
+	struct highres_timer
 	{
 	private:
 		highres_time t = highres_time::now();
@@ -20,6 +20,28 @@ namespace och
 			highres_time beg = t;
 
 			t = highres_time::now();
+
+			return beg - t;
+		}
+	};
+
+	struct timer
+	{
+	private:
+		time t = time::now();
+
+	public:
+
+		timespan read() const noexcept
+		{
+			return time::now() - t;
+		}
+
+		timespan reset() noexcept
+		{
+			time beg = t;
+
+			t = time::now();
 
 			return beg - t;
 		}
