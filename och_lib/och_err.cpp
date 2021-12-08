07045a5c1a530b2773584e344d228745e91cbde5
ochHerrje <och.herrje@gmail.com>
2021-12-08 01:52:53 +0100

    Fundamentally reworked och_err. status now carries error code and its type itself, instead of storing it via push_error. Furthermore, OCH_ERROR_CONTEXT_NONE does not carry the previous overheads for saving error_context's anymore and the macros for creating errors have been slightly reworked (without interfering with check()).

--- och_lib/och_err.cpp
+++ och_lib/och_err.cpp
@@ -4,21 +4,75 @@
 
 #if defined(OCH_USING_VULKAN)
 #include <vulkan/vulkan.h>
-#endif // defined(OCH_USING_VULKAN)
+#endif
+
+
+
+const char* och::error_context::filepath() const noexcept
+{
+#if defined(OCH_ERROR_CONTEXT_EXTENDED) || defined(OCH_ERROR_CONTEXT_NORMAL)
+	return m_filename;
+#else
+	return "[[Unknown]]";
+#endif
+}
+
+const char* och::error_context::filename() const noexcept
+{
+#if defined(OCH_ERROR_CONTEXT_EXTENDED) || defined(OCH_ERROR_CONTEXT_NORMAL)
+	size_t last_slash_or_backslash = 0;
+
+	for (size_t i = 0; m_filename[i] != '\0'; ++i)
+		if (m_filename[i] == '\\' || m_filename[i] == '/')
+			last_slash_or_backslash = i + 1;
+
+	return m_filename + last_slash_or_backslash;
+#else
+	return "[[Unknown]]";
+#endif
+}
+
+const char* och::error_context::function() const noexcept
+{
+#if defined(OCH_ERROR_CONTEXT_EXTENDED)
+	return m_function;
+#else
+	return "[[Unknown]]";
+#endif
+}
+
+const char* och::error_context::line_content() const noexcept
+{
+#if defined(OCH_ERROR_CONTEXT_EXTENDED)
+	return m_line_content;
+#else
+	return "[[Unknown]]";
+#endif
+}
+
+uint32_t och::error_context::line_number() const noexcept
+{
+#if defined(OCH_ERROR_CONTEXT_EXTENDED) || defined(OCH_ERROR_CONTEXT_NORMAL)
+	return m_line_number;
+#else
+	return 0;
+#endif
+}
+
 
 struct error_data_t
 {
-	static constexpr size_t MAX_CONTEXT_STACK_DEPTH = 16;
-
 	static constexpr size_t MAX_CUSTOM_MESSAGE_CHARS = 255;
+
+#if !defined(OCH_ERROR_CONTEXT_NONE)
+
+	static constexpr size_t MAX_CONTEXT_STACK_DEPTH = 16;
 
 	och::error_context m_context_stack[MAX_CONTEXT_STACK_DEPTH]{};
 
 	uint32_t m_context_stack_depth = 0;
 
-	och::error_type m_native_error_source = och::error_type::NONE;
-
-	uint64_t m_native_error_code = 0;
+#endif
 
 	char m_custom_message[MAX_CUSTOM_MESSAGE_CHARS + 1]{};
 };
@@ -27,59 +81,37 @@ thread_local error_data_t error_data;
 
 
 
-__declspec(noinline) void och::err::push_error(const error_context& ctx, uint64_t native_error_code, error_type native_error_source) noexcept
+#if !defined(OCH_ERROR_CONTEXT_NONE)
+__declspec(noinline) void och::err::push_error(const error_context& ctx) noexcept
 {
-	push_error(ctx, native_error_code, native_error_source, nullptr);
-}
-
-__declspec(noinline) void och::err::push_error(const error_context& ctx, uint64_t native_error_code, error_type native_error_source, const char* custom_message) noexcept
-{
-	if (!error_data.m_context_stack_depth)
-	{
-		error_data.m_context_stack[0] = ctx;
-
-		error_data.m_context_stack_depth = 1;
-
-		error_data.m_native_error_code = native_error_code;
-
-		error_data.m_native_error_source = native_error_source;
-
-		set_error_message(custom_message);
-	}
-	else if (error_data.m_context_stack_depth < error_data.MAX_CONTEXT_STACK_DEPTH)
+	if (error_data.m_context_stack_depth < error_data.MAX_CONTEXT_STACK_DEPTH)
 		error_data.m_context_stack[error_data.m_context_stack_depth++] = ctx;
 }
-
-
+#endif
 
 void och::err::reset_status() noexcept
 {
+#if !defined(OCH_ERROR_CONTEXT_NONE)
 	error_data.m_context_stack_depth = 0;
+#endif
 
 	error_data.m_custom_message[0] = '\0';
-
-	error_data.m_native_error_code = 0;
-
-	error_data.m_native_error_source = error_type::NONE;
-}
-
-uint64_t och::err::get_native_error_code() noexcept
-{
-	return error_data.m_native_error_code;
-}
-
-och::error_type och::err::get_error_type() noexcept
-{
-	return error_data.m_native_error_source;
 }
 
 och::range<const och::error_context> och::err::get_callstack() noexcept
 {
+#if !defined(OCH_ERROR_CONTEXT_NONE)
 	return och::range<const och::error_context>(error_data.m_context_stack, error_data.m_context_stack_depth);
+#else
+	return och::range<const och::error_context>(nullptr, nullptr);
+#endif
 }
 
 och::utf8_view och::err::get_error_message() noexcept
 {
+	if(!error_data.m_custom_message[0])
+		return och::utf8_view("[[No message registered]]");
+
 	return och::utf8_view(error_data.m_custom_message);
 }
 
@@ -98,9 +130,32 @@ void och::err::set_error_message(const char* msg) noexcept
 		error_data.m_custom_message[0] = '\0';
 }
 
-och::utf8_string och::err::get_error_description() noexcept
+
+
+uint32_t och::status::errcode() const noexcept { return static_cast<uint32_t>(m_errcode); }
+
+och::error_type och::status::errtype() const noexcept { return static_cast<och::error_type>(m_errcode >> 32); }
+
+och::utf8_view och::status::errtype_name() const noexcept
 {
-	switch (error_data.m_native_error_source)
+	switch (static_cast<och::error_type>(m_errcode >> 32))
+	{
+	case error_type::NONE:
+		return "NONE";
+	case error_type::och:
+		return "och::status";
+	case error_type::hresult:
+		return "HRESULT";
+	case error_type::vkresult:
+		return "VkResult";
+	}
+
+	return "[[Unknown]]";
+}
+
+och::utf8_string och::status::description() const noexcept
+{
+	switch (static_cast<och::error_type>(m_errcode >> 32))
 	{
 	case och::error_type::NONE:
 		return och::utf8_string("[[No error]]");
@@ -112,7 +167,7 @@ och::utf8_string och::err::get_error_description() noexcept
 	{
 		char* message_buffer;
 
-		if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, error_data.m_native_error_code & 0xFFFF, SUBLANG_NEUTRAL, reinterpret_cast<LPSTR>(&message_buffer), 0, nullptr))
+		if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, static_cast<DWORD>(m_errcode), SUBLANG_NEUTRAL, reinterpret_cast<LPSTR>(&message_buffer), 0, nullptr))
 			break;
 
 		och::utf8_string ret_str(message_buffer);
@@ -127,21 +182,4 @@ och::utf8_string och::err::get_error_description() noexcept
 	}
 
 	return "[[No description]]";
-}
-
-och::utf8_view och::err::get_error_type_name() noexcept
-{
-	switch (error_data.m_native_error_source)
-	{
-	case error_type::NONE:
-		return "NONE";
-	case error_type::och:
-		return "och::status";
-	case error_type::hresult:
-		return "HRESULT";
-	case error_type::vkresult:
-		return "VkResult";
-	}
-
-	return "[[Unknown]]";
 }
--- och_lib/och_err.h
+++ och_lib/och_err.h
@@ -8,35 +8,55 @@
 #include "och_utf8.h"
 #include "och_range.h"
 
+#if defined(OCH_ERROR_CONTEXT_EXTENDED)
+
+#pragma detect_mismatch("och_error_context", "extended")
+
+#endif
+
+#if defined(OCH_ERROR_CONTEXT_NONE)
+
+#pragma detect_mismatch("och_error_context", "none")
+
+#endif
+
+#if defined(OCH_ERROR_CONTEXT_NORMAL)
+
+#pragma detect_mismatch("och_error_context", "normal")
+
+#endif
+
+
+
+#if (defined(OCH_ERROR_CONTEXT_EXTENDED) && (defined(OCH_ERROR_CONTEXT_NONE) || defined(OCH_ERROR_CONTEXT_NORMAL))) || (defined(OCH_ERROR_CONTEXT_NORMAL) && defined(OCH_ERROR_CONTEXT_NONE))
+
+#pragma message("More than one of OCH_ERROR_CONTEXT_EXTENDED, OCH_ERROR_CONTEXT_NONE and OCH_ERROR_CONTEXT_NORMAL are defined. Falling back to OCH_ERROR_CONTEXT_NORMAL behaviour")
+
+#undef OCH_ERROR_CONTEXT_EXTENDED
+#undef OCH_ERROR_CONTEXT_NONE
+#undef OCH_ERROR_CONTEXT_NORMAL
+
+#define OCH_ERROR_CONTEXT_NORMAL
+
+#endif // (defined(OCH_ERROR_CONTEXT_EXTENDED) && (defined(OCH_ERROR_CONTEXT_NONE) || defined(OCH_ERROR_CONTEXT_NORMAL))) || (defined(OCH_ERROR_CONTEXT_NORMAL) && defined(OCH_ERROR_CONTEXT_NONE))
+
+
+
 #define CONSTEXPR_LINE_NUM_CAT_HELPER2(x, y) x##y
 
 #define CONSTEXPR_LINE_NUM_CAT_HELPER(x, y) CONSTEXPR_LINE_NUM_CAT_HELPER2(x, y)
 
 #define CONSTEXPR_LINE_NUM CONSTEXPR_LINE_NUM_CAT_HELPER(__LINE__, u)
 
+
+
 namespace och
 {
-#if (defined(OCH_ERROR_CONTEXT_EXTENDED) && defined(OCH_ERROR_CONTEXT_NONE)) || (defined(OCH_ERROR_CONTEXT_EXTENDED) && defined(OCH_ERROR_CONTEXT_NORMAL)) || (defined(OCH_ERROR_CONTEXT_NORMAL) && defined(OCH_ERROR_CONTEXT_NONE))
-
-	#pragma message("More than one of OCH_ERROR_CONTEXT_EXTENDED, OCH_ERROR_CONTEXT_NONE and OCH_ERROR_CONTEXT_NORMAL are defined. Falling back to OCH_ERROR_CONTEXT_NORMAL behaviour")
-	
-	#undef OCH_ERROR_CONTEXT_EXTENDED
-	#undef OCH_ERROR_CONTEXT_NONE
-	#undef OCH_ERROR_CONTEXT_NORMAL
-	
-	#define OCH_ERROR_CONTEXT_NORMAL
-	
-#endif // (defined(OCH_ERROR_CONTEXT_EXTENDED) && defined(OCH_ERROR_CONTEXT_NONE)) || (defined(OCH_ERROR_CONTEXT_EXTENDED) && defined(OCH_ERROR_CONTEXT_NORMAL)) || (defined(OCH_ERROR_CONTEXT_NORMAL) && defined(OCH_ERROR_CONTEXT_NONE))
-
-
-#if defined(OCH_ERROR_CONTEXT_EXTENDED)
-
-	#pragma detect_mismatch("och_error_context", "extended")
-
 	struct error_context
 	{
-	private:
+#if defined(OCH_ERROR_CONTEXT_EXTENDED)
 
+	private:
 		const char* m_filename;
 		const char* m_function;
 		const char* m_line_content;
@@ -49,72 +69,15 @@ namespace och
 		explicit error_context(const char* filename, const char* function, const char* line_content, uint32_t line_number) noexcept
 			: m_filename{ filename }, m_function{ function }, m_line_content{ line_content }, m_line_number{ line_number } {}
 
-		const char* filename() const noexcept
-		{
-			return m_filename;
-		}
-
-		const char* function() const noexcept
-		{
-			return m_function;
-		}
-
-		const char* line_content() const noexcept
-		{
-			return m_line_content;
-		}
-
-		uint32_t line_number() const noexcept
-		{
-			return m_line_number;
-		}
-	};
-
-	#define make_error_context(arg) och::error_context(__FILE__, __FUNCTION__, #arg, CONSTEXPR_LINE_NUM)
-
 #elif defined(OCH_ERROR_CONTEXT_NONE)
 
-	#pragma detect_mismatch("och_error_context", "none")
+	public:
 
-	struct error_context
-	{
 		explicit error_context() noexcept = default;
 
-		const char* filename() const noexcept
-		{
-			return "[[Unknown]]";
-		}
+#else // defined(OCH_ERROR_CONTEXT_NORMAL)
 
-		const char* function() const noexcept
-		{
-			return "[[Unknown]]";
-		}
-
-		const char* line_content() const noexcept
-		{
-			return "[[Unknown]]";
-		}
-
-		uint32_t line_number() const noexcept
-		{
-			return "[[Unknown]]";
-		}
-	};
-
-	#define make_error_context(line_content) och::error_context()
-
-#else
-	
-	#pragma detect_mismatch("och_error_context", "normal")
-	
-	#if !defined(OCH_ERROR_CONTEXT_NORMAL)
-		#pragma message("warning: None of OCH_ERROR_CONTEXT_NORMAL, OCH_ERROR_CONTEXT_EXTENDED and OCH_ERROR_CONTEXT_NONE are defined. Falling back to OCH_ERROR_CONTEXT_NORMAL behaviour.")
-	#endif // !defined(OCH_ERROR_CONTEXT_NORMAL)
-	
-	struct error_context
-	{
 	private:
-
 		const char* m_filename;
 		uint32_t m_line_number;
 
@@ -125,40 +88,42 @@ namespace och
 		explicit error_context(const char* filename, uint32_t line_number) noexcept
 			: m_filename{ filename }, m_line_number{ line_number } {}
 
-		const char* filename() const noexcept
-		{
-			return m_filename;
-		}
+#endif // defined(OCH_ERROR_CONTEXT_[EXTENDED|NONE|NORMAL])
 
-		const char* function() const noexcept
-		{
-			return "[[Unknown]]";
-		}
+		const char* filepath() const noexcept;
 
-		const char* line_content() const noexcept
-		{
-			return "[[Unknown]]";
-		}
+		const char* filename() const noexcept;
 
-		uint32_t line_number() const noexcept
-		{
-			return m_line_number;
-		}
+		const char* function() const noexcept;
+
+		const char* line_content() const noexcept;
+
+		uint32_t line_number() const noexcept;
 	};
+
+#if defined(OCH_ERROR_CONTEXT_EXTENDED)
+
+#define make_error_context(arg) och::error_context(__FILE__, __FUNCTION__, #arg, CONSTEXPR_LINE_NUM)
+
+#elif defined(OCH_ERROR_CONTEXT_NONE)
+
+#define make_error_context(line_content) och::error_context()
+
+#else
 
 #define make_error_context(line_content) och::error_context(__FILE__, CONSTEXPR_LINE_NUM)
 
-#endif // defined(OCH_ERROR_CONTEXT_[EXTENDED, NONE, NORMAL])
+#endif
 
-#define check(arg) if(och::status s = och::to_status(arg, make_error_context(arg)); s) return s;
+#define check(arg) do { if(och::status s = och::register_status(och::to_status(arg), make_error_context(arg))) return s; } while (false)
 
-#define check_msg(arg, msg) if(och::status s = och::to_status(arg, make_error_context(arg), msg) return s;
+#define check_msg(arg, msg) do { if(och::status s = och::register_status(och::to_status(arg), maker_error_context(arg), msg)) return s; } while (false)
 
-#define make_status(arg) och::to_status(arg, make_error_context(arg))
+#define error(arg) do { return och::register_status(och::to_status(arg), make_error_context(arg)); } while (false)
 
-#define make_status_msg(arg, msg) och::to_status(arg, make_error_context(arg), msg)
+#define error_msg(arg, msg) do { return och::register_status(och::to_status(arg), make_error_context(arg), msg); } while (false)
 
-#define msg_error(msg) och::to_status(make_error_context([[Unknown]]), msg)
+#define msg_error(msg) static_assert(false, "The msg_error is no longer supported.")
 
 #define ignore_status(s) if(s) och::err::reset_status();
 
@@ -170,17 +135,36 @@ namespace och
 		vkresult = 3,
 	};
 
+	struct status
+	{
+	private:
+
+		uint64_t m_errcode;
+
+	public:
+
+		operator bool() const noexcept { return static_cast<uint32_t>(m_errcode); }
+
+		__forceinline status() noexcept : m_errcode{ 0ull } {}
+
+		__forceinline explicit status(uint64_t errcode) noexcept : m_errcode{ errcode } {}
+
+
+
+		uint32_t errcode() const noexcept;
+
+		och::error_type errtype() const noexcept;
+
+		och::utf8_view errtype_name() const noexcept;
+
+		och::utf8_string description() const noexcept;
+	};
+
 	namespace err
 	{
-		__declspec(noinline) void push_error(const error_context& ctx, uint64_t native_error_code, error_type native_error_source) noexcept;
-
-		__declspec(noinline) void push_error(const error_context& ctx, uint64_t native_error_code, error_type native_error_source, const char* custom_message) noexcept;
+		__declspec(noinline) void push_error(const error_context& ctx) noexcept;
 
 		void reset_status() noexcept;
-
-		uint64_t get_native_error_code() noexcept;
-
-		error_type get_error_type() noexcept;
 
 		och::range<const error_context> get_callstack() noexcept;
 
@@ -188,50 +172,42 @@ namespace och
 
 		void set_error_message(const char* msg) noexcept;
 
-		och::utf8_string get_error_description() noexcept;
-
-		och::utf8_view get_error_type_name() noexcept;
 	}
 
-	struct status
+	__forceinline status to_status() noexcept
 	{
-	private:
+		return status(1ull | (static_cast<uint64_t>(error_type::och) << 32));
+	}
 
-		bool m_is_bad;
-		
-	public:
+	__forceinline status to_status(status rst) noexcept
+	{
+		return rst;
+	}
 
-		operator bool() const noexcept
+	__forceinline status register_status(status s, const och::error_context& ctx) noexcept
+	{
+#if !defined(OCH_ERROR_CONTEXT_NONE)
+		if (s)
+			err::push_error(ctx);
+#else
+		ctx;
+#endif
+		return s;
+	}
+
+	__forceinline status register_status(status s, const och::error_context& ctx, const char* message) noexcept
+	{
+		if (s)
 		{
-			return m_is_bad;
+#if !defined(OCH_ERROR_CONTEXT_NONE)
+			err::push_error(ctx);
+#else
+			ctx;
+#endif
+			err::set_error_message(message);
 		}
 
-		__forceinline status() noexcept : m_is_bad{ false } {}
-
-		__forceinline explicit status(bool is_bad) noexcept : m_is_bad{ is_bad } {}
-	};
-
-	__forceinline status to_status(status rst, const error_context& ctx) noexcept
-	{
-		if (rst)
-			err::push_error(ctx, 1ull, error_type::och);
-
-		return rst;
-	}
-
-	__forceinline status to_status(status rst, const error_context& ctx, const char* custom_message) noexcept
-	{
-		if (rst)
-			err::push_error(ctx, 1ull, error_type::och, custom_message);
-
-		return rst;
-	}
-
-	__forceinline status to_status(const error_context& ctx, const char* custom_message) noexcept
-	{
-		err::push_error(ctx, 1ull, error_type::och, custom_message);
-
-		return status(true);
+		return s;
 	}
 }
 
@@ -241,20 +217,9 @@ namespace och
 #define OCH_ERR_WINDOWS_INTEROP_INCLUDE_GUARD
 namespace och
 {
-	__forceinline status to_status(HRESULT rst, const error_context& ctx) noexcept
+	__forceinline status to_status(HRESULT rst) noexcept
 	{
-		if (rst != S_OK)
-			err::push_error(ctx, static_cast<uint64_t>(static_cast<uint32_t>(rst)), error_type::hresult);
-
-		return status(rst != S_OK);
-	}
-
-	__forceinline status to_status(HRESULT rst, const error_context& ctx, const char* custom_message) noexcept
-	{
-		if (rst != S_OK)
-			err::push_error(ctx, static_cast<uint64_t>(static_cast<uint32_t>(rst)), error_type::hresult, custom_message);
-
-		return status(rst != S_OK);
+		return status(static_cast<uint32_t>(rst) | (static_cast<uint64_t>(error_type::hresult) << 32));
 	}
 }
 #endif // !defined(OCH_ERR_WINDOWS_INTEROP_INCLUDE_GUARD) && defined(_WINDOWS_)
@@ -263,20 +228,9 @@ namespace och
 #define OCH_ERR_VULKAN_INTEROP_INCLUDE_GUARD
 namespace och
 {
-	__forceinline status to_status(VkResult rst, const error_context& ctx) noexcept
+	__forceinline status to_status(VkResult rst) noexcept
 	{
-		if (rst != VK_SUCCESS)
-			err::push_error(ctx, static_cast<uint64_t>(static_cast<uint32_t>(rst)), error_type::vkresult);
-
-		return status(rst != VK_SUCCESS);
-	}
-
-	__forceinline status to_status(VkResult rst, const error_context& ctx, const char* custom_message) noexcept
-	{
-		if (rst != VK_SUCCESS)
-			err::push_error(ctx, static_cast<uint64_t>(static_cast<uint32_t>(rst)), error_type::vkresult, custom_message);
-
-		return status(rst != VK_SUCCESS);
+		return status(static_cast<uint32_t>(rst) | (static_cast<uint64_t>(error_type::vkresult) << 32));
 	}
 }
 #endif // !defined(OCH_ERR_VULKAN_INTEROP_INCLUDE_GUARD) && defined(OCH_USING_VULKAN) && (defined(VULKAN_CORE_H_) || defined(VULKAN_H_))
